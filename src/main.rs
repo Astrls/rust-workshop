b8147e4fdf667a8590d49ddd4d1dac29c7af734a
Antoine Struelens <antoine.struelens@gmail.com>
2023-10-11 15:28:14 +0200

    axolotl speech bubble (3 versions) with flags -sad -mad

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+/// A little CLI that outputs a message.
+struct Arguments {
+    ///The thing to say.
+    message: String,
+    
+    #[clap(short, long)]
+    mad: bool,
+
+    #[clap(short, long)]
+    sad: bool,
+}
+
+fn main(){
+    let args = Arguments::parse();
+    //println!("{}",args.message);
+
+    let message = &args.message;
+    let dashes = "-".repeat(message.len() + 2);
+    println!("         +{dashes}+");
+    println!("         | {message} |");
+    println!("         +{dashes}+");
+    println!("        /");
+
+    let mad: bool = args.mad;
+    if mad == true {
+        println!("(╯°□°)╯︵ ┻━┻");
+    } else if args.sad == true{
+       println!("≽( ಡ_ಡ )≼");
+    } else {
+        println!("≽(◕ ᴗ ◕)≼");
+    }
 }
